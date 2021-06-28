4a9fb1f14171da5957bd075dfede597ccfec8580
PileroiD <liderfrakzii@gmail.com>
2021-06-28 09:23:09 +0300

    third commit

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('comment/<int:pk>/', views.CreateComment.as_view(), name='create_comment'),
     path('', views.HomeView.as_view(), name='home'),
     path('<slug:slug>/', views.PostListView.as_view(), name='post_list'),
     path('<slug:slug>/<slug:post_slug>/', views.PostDetailView.as_view(), name='post_detail'),
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
 import random
 
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 
 from blog import models
+from blog.forms import CommentForm
+from blog.models import Comments
 
 
 class HomeView(ListView):
@@ -27,3 +29,39 @@ class PostDetailView(DetailView):
     model = models.Post
     slug_url_kwarg = 'post_slug'
     context_object_name = 'post'
+    extra_context = {'form': CommentForm()}
+
+
+class CreateComment(CreateView):
+    model = Comments
+    form_class = CommentForm
+    success_url = '/'
+
+    def form_valid(self, form):
+        # print('========================')
+        # print(form.instance.post_id) -> None
+        # print(self.kwargs.get('pk')) -> post id
+        # print(self.object.__class__) -> <class 'NoneType'>
+        # print('========================')
+
+        form.instance.post_id = self.kwargs.get('pk')  # создаем инстанцию модели коментариев ( Comments ) и привязываем к ней id поста из которого был отправлен отзыв
+        # print(form.instance.post_id) -> post id
+        # print(form.instance) -> Comments object (None)
+        # print('========================')
+        # print(form) -> html формы
+        # print('========================')
+
+        self.object = form.save()
+        # print('========================')
+        # print(form.save()) -> Comments object (post id)
+        # print('========================')
+
+        # print('========================')
+        # print(self.object) -> Comments object (post id)
+        # print(super().form_valid(form)) -> <HttpResponseRedirect status_code=302, "text/html; charset=utf-8", url="/">
+        # print('========================')
+
+        return super().form_valid(form)  # перенаправляем на какой-то урл
+
+    def get_success_url(self):
+        return self.object.post.get_absolute_url()
